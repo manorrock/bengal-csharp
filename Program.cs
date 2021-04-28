8dd08f6c6276627440ff92340c34bfa0685911b5
Manfred Riem <22894110+manorrock@users.noreply.github.com>
2021-04-28 13:38:48 +0000

    Fixes issue #5 - Translate each .bmesg file to a .cs file

--- Program.cs
+++ Program.cs
@@ -1,12 +1,48 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
 
-namespace bengal_message_csharp
+namespace bmesg2csharp
 {
     class Program
     {
+        //
+        // Call
+        //
+        static void Call(string[] sourceFiles)
+        {
+            if (sourceFiles != null && sourceFiles.Length > 0) 
+            {
+                foreach (string sourceFile in sourceFiles)
+                {
+                    CompileFile(sourceFile);
+                }
+            }
+            else
+            {
+                Console.WriteLine("No source files found");
+            }
+        }
+
+        //
+        // Compile the given file.
+        //
+        static void CompileFile(string sourceFile)
+        {
+            Console.WriteLine("Compiling - " + sourceFile);
+            string sourceContent = System.IO.File.ReadAllText(sourceFile);
+            string destinationFilename = sourceFile;
+            destinationFilename = destinationFilename.Substring(0, destinationFilename.LastIndexOf("."));
+            destinationFilename = destinationFilename + ".cs";
+            File.WriteAllText(destinationFilename, sourceContent);
+        }
+
+        //
+        // Main method.
+        //
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Call(args);
         }
     }
 }
